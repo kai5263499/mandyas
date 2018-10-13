4e17610e87cba7ebbdab902ded669393e2b32520
Wes Widner <kai5263499@gmail.com>
2018-10-13 19:00:52 -0400

    prepare for client/server grpc implementations

--- server/grpc.go
+++ server/grpc.go
@@ -1,4 +1,4 @@
-package domain
+package server
 
 import (
 	"fmt"
@@ -9,12 +9,13 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
+	"github.com/kai5263499/mandyas/domain"
 	pb "github.com/kai5263499/mandyas/generated"
 )
 
 type Server struct {
 	Cmd           *exec.Cmd
-	Conf          *Config
+	Conf          *domain.Config
 	ServiceServer pb.MandyasServiceServer
 	lis           *net.Listener
 	grpcServer    *grpc.Server
